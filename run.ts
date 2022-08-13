e9fe5d2c364d2b5b59c13ab93aaff2b1b0b87f3b
pan <robertpanvip@163.com>
2022-08-13 16:53:06 +0800

    feat:修改直接执行ts

--- run.ts
+++ run.ts
@@ -2,7 +2,7 @@
 import path from 'path';
 import spawn from 'cross-spawn';
 
-const executePath = path.join(process.cwd(), "./src/es-pkg.ts")
+const executePath = path.join(__dirname, "../src/es-pkg.ts")
 
 spawn('esno', [executePath, ...process.argv.slice(2)], {
     env: process.env,
