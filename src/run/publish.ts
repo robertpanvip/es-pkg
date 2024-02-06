4d5476e6abe5553eb40f93d8aca510fa9f961ca1
robertpan <robertpanvip@163.com>
2024-02-06 10:52:28 +0800

    feat:完善发布流程

--- src/run/publish.ts
+++ src/run/publish.ts
@@ -3,12 +3,15 @@ import chalk from 'chalk';// 改变屏幕文字颜色
 import logger from '../logger'
 import fs from "fs"
 import path from "path";
-import {autoUpgrade, compare, remove, run} from "../utils/util";
-import {error, log, success} from "../utils/log";
-import {config, pkg} from "../utils/config";
+import { autoUpgrade, compare, remove, run } from "../utils/util";
+import { error, log, success } from "../utils/log";
+import { config, pkg } from "../utils/config";
+import prompts from "prompts"
 
 const scoped = /^@[a-zA-Z0-9-]+\/.+$/;
 const REGISTRY = "https://registry.npmjs.org"
+
+const json: Record<string, string | object> = pkg;
 
 gulp.task('clean', async (cb) => {
     log(`清除${config.publishDir}开始`)
@@ -36,8 +39,8 @@ gulp.task('del-dist', async (cb) => {
 });
 gulp.task('copy-info', async () => {
     log(`生成 package 开始`)
-    const json: Record<string, string | object> = pkg;
-    const {default: fetch} = await import("node-fetch")
+    const { default: fetch } = await import("node-fetch");
+    let errored = false;
     try {
         const response = await fetch(`https://registry.npmjs.org/${pkg.name}`)
         const res = await response.json() as { "dist-tags": { latest: string }, "error": string };
@@ -49,12 +52,20 @@ gulp.task('copy-info', async () => {
             if (res["dist-tags"]) {
                 json.version = compare(pkg.version, res["dist-tags"].latest) <= 0 ? autoUpgrade(res["dist-tags"].latest) : pkg.version
             } else {
-                error(`获取版本号失败`)
-                json.version = pkg.version;
+                errored = true;
             }
         }
     } catch (e) {
-        error(`获取版本号失败`, e)
+        errored = true;
+    }
+    if (errored) {
+        const { version } = await prompts({
+            type: 'text',
+            name: 'version',
+            message: '远程获取版本失败！请输入版本号',
+            initial: pkg.version
+        });
+        json.version = version;
     }
 
     delete json.devDependencies;
@@ -74,7 +85,7 @@ gulp.task('copy-info', async () => {
     let jsonStr = JSON.stringify(json, null, "\t")
     const ex = fs.existsSync(`${config.publishDir}/`)
     if (!ex) {
-        fs.mkdirSync(`${config.publishDir}/`, {recursive: true})
+        fs.mkdirSync(`${config.publishDir}/`, { recursive: true })
     }
     fs.writeFileSync(`${config.publishDir}/package.json`, jsonStr)
     log(`生成 package完成`, chalk.green(json.version))
@@ -133,16 +144,26 @@ gulp.task('npm-publish', async function () {
         publishAccess = config.publishAccess;
     }
     try {
-        const {stdout} = await run(`npm`, ["whoami", '--registry', REGISTRY], {stdio: undefined})
+        const { stdout } = await run(`npm`, ["whoami", '--registry', REGISTRY], { stdio: undefined })
         log.warn(`===npm登录信息===>${stdout}`)
     } catch (e) {
         log.warn("===npm未登录！！请登录")
         await run(`npm`, ["login", '--registry', REGISTRY])
+    };
+    const { confirm } = await prompts({
+        type: 'confirm',
+        name: 'confirm',
+        message: '是否发布？',
+    });
+
+    if (confirm) {
+        await run(`npm`, ['publish', ...publishAccess, '--registry', REGISTRY], {
+            cwd: path.join(process.cwd(), config.publishDir),
+        });
+        success(`${json.name}@${json.version}:发布成功！`)
+    } else {
+        error(`${json.name}@${json.version}:取消成功！`)
     }
-     await run(`npm`, ['publish', ...publishAccess, '--registry', REGISTRY], {
-         cwd: path.join(process.cwd(), config.publishDir),
-     });
-    success(["npm", "publish", ...publishAccess].join(' '))
 });
 
 export default gulp.series('clean', 'del-dist', 'copy-info', 'copy-dist', 'copy-es', 'copy-lib', 'npm-publish')
